06ecc08ba4e3d4278013982661c4f1804d8bb5c1
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-29 12:07:06 -0300

    6.5 - 6.6: order by votes, action creators

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -3,6 +3,7 @@ import { createAnecdote, addVote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const vote = (id) => {
