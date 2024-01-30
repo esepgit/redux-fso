2fb96776756310294ea40eca2fe725d389b36070
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-30 10:04:03 -0300

    6.10: configureStore

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,16 @@
-const filterReducer = (state = "ALL", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.filter;
-    default:
-      return state;
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = 'ALL'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      return action.payload
+    }
   }
-};
+})
 
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    filter,
-  };
-};
-
-export default filterReducer;
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer;
